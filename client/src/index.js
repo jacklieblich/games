16593a934d8b5364fe1d67c15cd1b23474a0a84a
Jack Lieblich <jacklieb220@gmail.com>
2017-12-18 16:52:16 -0500

    prefix urls with # for direct links

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render((
-  <BrowserRouter>
+  <BrowserRouter basename="/#/">
     <App />
   </BrowserRouter>
 ), document.getElementById('root'))
