78e53adeae0f0785ea5c7ab970859be4496e5ce0
Jack Lieblich <jacklieb220@gmail.com>
2017-12-25 19:02:46 -0500

    style dashboard header

--- client/src/Components/app.js
+++ client/src/Components/app.js
@@ -20,7 +20,12 @@ class App extends React.Component {
 	render() {
 
 		if (this.state.loading) {
-			return <p>loading</p>
+			return (
+				<div className="spinner">
+				  <div className="double-bounce1"></div>
+				  <div className="double-bounce2"></div>
+				</div>
+			);
 		}
 
 		return (
