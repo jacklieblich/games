68b2be1e719db48e1fada4efca3bad5a86b2b198
Jack Lieblich <jacklieb220@gmail.com>
2017-12-26 23:04:00 -0500

    add privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'games#index'
 
+  get '/privacy', to: 'application#privacy'
+
   scope '/api' do
     post :challenge, to: 'games#create'
     get :users, to: 'users#index'
