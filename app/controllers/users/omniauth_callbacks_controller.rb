32faf43292bd9324a2c6da84813789e6c64ba954
Jack Lieblich <jacklieb220@gmail.com>
2018-01-15 18:37:17 -0500

    Keep user logged in.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,13 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
+	def facebook
+		@user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
-      sign_in(@user)
-      redirect_to env['omniauth.origin'] || root_path
-    else
-      render :status => 401, :json => { :errors => alert }
-    end
-  end
+		if @user.persisted?
+			@user.remember_me = true
+			sign_in(@user)
+			redirect_to env['omniauth.origin'] || root_path
+		else
+			render :status => 401, :json => { :errors => alert }
+		end
+	end
 end
