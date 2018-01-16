23cdbd22ababd1f532061313201de7170d9d4fe4
Jack Lieblich <jacklieb220@gmail.com>
2018-01-15 23:55:45 -0500

    Change Hex orientation on mobile.

--- client/src/Components/Games/Hex/HexPiece.js
+++ client/src/Components/Games/Hex/HexPiece.js
@@ -2,11 +2,29 @@ import React, { Component } from 'react';
 import Hexagon from 'react-hexagon'
 class HexPiece extends Component {
 
+	updateDimensions() {
+        this.setState({width: window.innerWidth});
+    }
+
+    componentWillMount() {
+        this.updateDimensions();
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.updateDimensions.bind(this));
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateDimensions.bind(this));
+    }
+
   render() {
+  	console.log(this.state)
     return (
       <Hexagon
         className={this.props.className}
         onClick={this.props.onClick}
+        flatTop={this.state.width > 767 ? false : true}
       />
     );
 
